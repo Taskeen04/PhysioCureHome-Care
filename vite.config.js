07aac46b75976371896b596e72919734987a4ceb
Taskeen Begum <taskeenbegum04@gmail.com>
2026-07-10 14:33:09 +0530

    Optimize Vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,26 @@ export default defineConfig({
   build: {
     cssCodeSplit: true,
     sourcemap: false,
-    chunkSizeWarningLimit: 1000,
+    chunkSizeWarningLimit: 800,
+    rollupOptions: {
+      output: {
+        // Break bundling monoliths down to highly-cacheable progressive ES modules
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react-dom') || id.includes('react')) {
+              return 'vendor-react';
+            }
+            if (id.includes('react-icons')) {
+              return 'vendor-icons';
+            }
+            return 'vendor-others';
+          }
+        }
+      }
+    }
+  },
+  esbuild: {
+    // Automatically strip dev-only debugger states and console warnings from production builds
+    drop: ['console', 'debugger'],
   }
 })
